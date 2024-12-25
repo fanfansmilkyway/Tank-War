cd49df62981fa5ed91825f3c8ea7a4d62bdeffd7
fanfansmilkyway <fanfansmilkyway@qq.com>
2024-12-25 21:44:27 +0800

    Communication: Basic echo message exchanging

--- communication/client/client.py
+++ communication/client/client.py
@@ -1,5 +1,12 @@
 import socket
 
+FORMAT = 'utf-8'
 ADDR = ("127.0.0.1", 8080) # Server IP
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+print("[CONNECTION] Successfully connected with the server")
+
+msg = client.recv(2024).decode(FORMAT)
+print(msg)
+client.send(b"Here is the client")
--- communication/server/server.py
+++ communication/server/server.py
@@ -1,9 +1,23 @@
 import socket
+import threading
 
+FORMAT = 'utf-8'
 ADDR = ("127.0.0.1", 8080)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
+
+def handle_client(client:socket.socket):
+    client.send(b"Hello, I'm from the server")
+    msg = client.recv(2024).decode(FORMAT)
+    print(msg)
+
+# First estabilish the connection with the client
 server.listen()
-conn, addr = server.accept()
-print(f"Successfully connected with {conn}")
+print(f"[LITSEN]Server is now listening on {ADDR}")
+while True:
+    conn, addr = server.accept()
+    thread = threading.Thread(target=handle_client, args=(conn,))
+    thread.start()
+    print(f"[CONNECTION]Successfully connected with {conn}")
+
